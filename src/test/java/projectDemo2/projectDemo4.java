63cde033d9c47b770c0d783dabc6a48648373d40
Asush <Asush@DESKTOP-CEVN84Q>
2022-09-11 20:53:19 +0530

    project 2 test cases

---
+++ src/test/java/projectDemo2/projectDemo4.java
@@ -0,0 +1,8 @@
+package projectDemo2;
+
+public class projectDemo4 {
+public static void main(String[] args) {
+	
+	System.out.println("Nashik");
+}
+}
---
+++ src/test/java/projectDemo2/projectDemo_2.java
@@ -0,0 +1,8 @@
+package projectDemo2;
+
+public class projectDemo_2 {
+
+	public static void main(String[] args) {
+		System.out.println("Shiradi");
+	}
+}
